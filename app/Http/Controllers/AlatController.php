10c0c5c890865d76b49f7133e5e9e5f400f74c39
KiwilDev <boskhilmiarked123@gmail.com>
2024-03-23 11:47:36 +0700

    .

--- app/Http/Controllers/AlatController.php
+++ app/Http/Controllers/AlatController.php
@@ -57,11 +57,45 @@ class AlatController extends Controller
         }
     }
 
-    public function update() {
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all, [
+            'alat_kategori_id'=> 'required|int',
+            'alat_nama'=> 'required|string|max:150',
+            'alat_deskripsi'=> 'required|string|max:255',
+            'alat_hargaperhari'=> 'required|int',
+            'alat_stok'=> 'required|int',
+        ]);
 
+        if($validator->fails()) {
+            return response()->json([
+                'msg'=> 'Gagal mengubah data kategori',
+                'errors'=> $validator->errors()
+            ],422);
+        } else {
+            $alat = $this->alatModel->update_alat($validator->validated(), $id);
+
+            return response()->json([
+                'msg'=> 'Berhasil mengubah data alat',
+                'data'=> $alat
+            ],200);
+        }
     }
 
-    public function destroy() {
+    public function destroy($id)
+    {
+        $alat = $this->alatModel->delete_alat($id);
 
+        if(!$alat) {
+            return response()->json([
+                'msg' => 'Gagal menghapus data alat',
+                'errors' => 'Data tidak ditemukan'
+            ],404);
+        } else {
+            return response()->json([
+                'msg'=> 'Berhasil menghapus data alat',
+                'data'=> $alat
+            ],200);
+        }
     }
 }
--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Pelanggan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PelangganController extends Controller
 {
@@ -11,5 +12,91 @@ class PelangganController extends Controller
 
     public function __construct() {
         $this->pelangganModel = new Pelanggan();
+    }
+
+    public function index()
+    {
+        $pelanggan = $this->pelangganModel->get_pelanggan();
+
+        if(count($pelanggan) === 0){
+            return response()->json([
+                'msg'=>"Data pelanggan masih kosong!",
+                'data'=>$pelanggan
+            ],204);
+        } else {
+            return response()->json([
+                'data'=>$pelanggan
+            ],200);
+        }
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'pelanggan_nama'=> 'required|string|max:100',
+            'pelanggan_alamat' => 'required|string|max:100',
+            'pelanggan_notelp' => 'required|string|max:100',
+            'pelanggan_email' => 'required|string|max:100',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'msg'=> 'Gagal menambahkan data pelanggan',
+                'errors'=> $validator->errors()
+            ],422);
+        } else {
+            $pelanggan = $this->pelangganModel->create_pelanggan($validator->validated());
+
+            return response()->json([
+                'msg' => 'Berhasil menambahkan data pelanggan',
+                'data' => $pelanggan
+            ],201);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all, [
+            'pelanggan_nama'=> 'required|string|max:100',
+            'pelanggan_alamat' => 'required|string|max:100',
+            'pelanggan_notelp' => 'required|string|max:100',
+            'pelanggan_email' => 'required|string|max:100',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'msg'=> 'Gagal mengubah data pelanggan',
+                'errors'=> $validator->errors()
+            ],422);
+        } else {
+            $pelanggan = $this->pelangganModel->update_pelanggan($validator->validated(), $id);
+
+            return response()->json([
+                'msg'=> 'Berhasil mengubah data pelanggan',
+                'data'=> $pelanggan
+            ],200);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $pelanggan = $this->pelangganModel->delete_pelanggan($id);
+
+        if(!$pelanggan) {
+            return response()->json([
+                'msg' => 'Gagal menghapus data pelanggan',
+                'errors' => 'Data tidak ditemukan'
+            ],404);
+        } else {
+            return response()->json([
+                'msg'=> 'Berhasil menghapus data pelanggan',
+                'data'=> $pelanggan
+            ],200);
+        }
     }
 }
